ab92881bfe6efb023c8aa0b6ff33a7f8f92f060e
WangYilin <wangyilinv@gmail.com>
2019-06-13 23:17:18 +0800

    First commit

---
+++ Utils.java
@@ -0,0 +1,29 @@
+package com.example.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class Utils {
+
+    private static Toast toast;
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+    public static void showToast(Context context, String content) {
+
+        if (toast == null) {
+            toast = Toast.makeText(context, content, Toast.LENGTH_SHORT);
+        } else {
+            toast.setText(content);
+        }
+        toast.show();
+    }
+
+}
